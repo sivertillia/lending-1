81e9114cd2e784d67db50b984a306499c490b8e4
sivertillya <volk.sivert@gmail.com>
2021-06-07 23:24:21 +0300

    add two block - the end

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function() {
-    // alert('Hello, World!')
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+        loop:false,
+        dots:false,
+        nav:false,
+        mouseDrag:false,
+        margin:10,
+        items:1,
+        URLhashListener:true,
+        autoplayHoverPause:true,
+        startPosition: 'URLHash'
+    })
+    $('.tab_unit_js').click( function() { 
+        $(this).addClass('active').siblings().removeClass('active');
+    })
+    
 });
